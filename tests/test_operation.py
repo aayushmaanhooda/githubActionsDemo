95649c08d6899771a0c5fce699f0ad046747b5c3
aayushmaanhooda <aayushmaan162@gmail.com>
2025-12-29 12:19:26 +1100

    added new test cases

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -3,8 +3,10 @@ from src.opertation import add, sub
 def test_add():
     assert add(2,3) == 5
     assert add(-1,1) == 0
+    assert add(10,10) == 20
 
 
 def test_sub():
     assert sub(5,3) == 2
     assert sub(1,1) == 0
+    assert sub(10,1) == 9
